71135d9657d963bf2c5e8c77710be0cd909b0695
Siemen Braeckman <siemen_braeckman@hotmail.com>
2019-05-21 12:46:39 +0200

    ...

---
+++ src/LabelCPanel.java
@@ -0,0 +1,13 @@
+public class LabelCPanel extends CreatorPanel {
+    public LabelCPanel(FormListener formListener, PrintListener printListener)
+    {
+        super("Label creator", formListener, printListener);
+
+        addInfoLabel(20);
+    }
+
+    private void makeButtons()
+    {
+
+    }
+}
